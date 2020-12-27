eb8d4f058988890f1e7f8f1e399cbb53d25732c3
Oleg Maksimchik <olegmr1991@gmail.com>
2020-12-27 17:43:38 +0300

    before deploy

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
-const PORT = config.get('port')
+const PORT = config.get('port') || 5000
 
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/users', require('./routes/users.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 async function start() {
   try {
